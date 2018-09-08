6c751e945cce4f61f06298d0d658cfdb88d9ba77
Nelson Ameyo <nelson@lipasafe.com>
2018-09-09 01:15:40 +0300

    This is an automated deployment

--- routes/api.php
+++ routes/api.php
@@ -11,7 +11,7 @@ Route::post('user/login', 'Auth\LoginController@apiLogin');
 Route::post('login', 'API\AccountController@login');
 Route::post('signup', 'API\AccountController@signup');
 Route::get('me', 'API\AccountController@me')->middleware('jwt.auth');
-Route::post('location/update', 'API\AccountController@updateLocation');
+Route::post('location/update', 'API\AccountController@updateLocation')->middleware('jwt.auth');
 
 Route::resource('ads', 'API\AdsController');
 
