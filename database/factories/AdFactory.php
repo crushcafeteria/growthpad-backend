5555cebf3261c1f65b3dbc9e9a56aaf877b072ed
Nelson Ameyo <nelson@lipasafe.com>
2018-09-09 22:25:38 +0300

    This is an automated deployment

--- database/factories/AdFactory.php
+++ database/factories/AdFactory.php
@@ -13,9 +13,9 @@ $factory->define(\App\Models\Ad::class, function (Faker $faker){
         'email'        => $faker->freeEmail,
         'location'     => 'Nairobi',
         'pictures'     => [
-            'http://placehold.it/500x400/' . str_replace('#', null, $faker->hexcolor) . '?text=Product',
-            'http://placehold.it/500x400/' . str_replace('#', null, $faker->hexcolor) . '?text=Product',
-            'http://placehold.it/500x400/' . str_replace('#', null, $faker->hexcolor) . '?text=Product',
+            'http://placehold.it/500x400/' . str_replace('#', null, $faker->hexcolor) . '/ffffff/?text=Product',
+            'http://placehold.it/500x400/' . str_replace('#', null, $faker->hexcolor) . '/ffffff/?text=Product',
+            'http://placehold.it/500x400/' . str_replace('#', null, $faker->hexcolor) . '/ffffff/?text=Product',
         ],
         'status'       => 'ACTIVE',
         'expiry'       => \Carbon\Carbon::now()->addMonths(12)
