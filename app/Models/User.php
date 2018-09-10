9356a1d05c931ced83cd0c81d2a4279e7c16f93b
Nelson Ameyo <nelson@lipasafe.com>
2018-09-11 00:45:28 +0300

    This is an automated deployment

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,7 +57,7 @@ class User extends Authenticatable implements JWTSubject
         if (!$val) {
             return 'http://placehold.it/300?Image coming soon';
         } else {
-            return $val;
+            return asset($val);
         }
     }
 }
