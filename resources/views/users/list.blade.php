76d67d3d88c5c54f2797d255db5e17aebc06063f
Nelson Ameyo <nelson@Nelsons-MacBook-Pro.local>
2019-01-21 14:30:15 +0300

    This is an automated deployment

--- resources/views/users/list.blade.php
+++ resources/views/users/list.blade.php
@@ -30,7 +30,7 @@
                                 @foreach($accounts as $user)
                                     <tr>
                                         <td>
-                                            <img src="{{ (!$user->picture) ? 'http://placehold.it/30x30/000/fff?text='.substr($user->name, 0, 1) : $user->picture }}" width="50" class="img-circle">
+                                            <img src="{{ (!$user->picture) ? 'http://placehold.it/30x30/000/fff?text='.substr($user->name, 0, 1) : $user->picture }}" width="50" class="img-rounded">
                                         </td>
                                         <td>{{ $user->name }}</td>
                                         <td>{{ $user->gender }}</td>
