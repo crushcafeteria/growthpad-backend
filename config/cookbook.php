8c0c9d9c7ea6e73275cc9ab3705160510d948c99
Nelson Ameyo <nelson@lipasafe.com>
2020-04-01 08:22:52 +0300

    Updating cookbook images

--- config/cookbook.php
+++ config/cookbook.php
@@ -10,103 +10,103 @@ return [
 
     'products' => [
         [
-            'picture' => '/recipe/products/Lydia Okumu - Spider Plant, Cowpeas, Slender leaf, and jute mallow.jpg',
+            'picture' => '/recipe/products/18-Spider-Plant-(Chisaka).jpg',
             'file'    => 'recipes/CHISAKA - Spider Plant.pdf',
             'name'    => 'CHISAKA - Spider Plant',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/6-Boiled-Narrow-Leaf-(Emiroo).jpg',
             'file'    => 'recipes/EMIROO - Boiled Narrow Leaf.pdf',
             'name'    => 'EMIROO - Boiled Narrow Leaf',
             'price'   => 100
         ],
         [
-            'picture' => '/recipe/products/Josephine Ochomo - Dried Fish (Shibambala).jpg',
+            'picture' => '/recipe/products/7-Dried-Fish-(Eshibambala).jpg',
             'file'    => 'recipes/ESHIBAMBALA - Dried Fish.pdf',
             'name'    => 'ESHIBAMBALA - Dried Fish',
             'price'   => 100
         ],
         [
-            'picture' => '/recipe/products/Fredrick Okwaro - Bakhayo mashed banana (Litore), Smoked fish and cowpeas (Sikhubi).jpg',
+            'picture' => '/recipe/products/16-Smoked-Fish-(Eshibambala).jpg',
             'file'    => 'recipes/ESHIBAMBALA - Smoked Fish.pdf',
             'name'    => 'ESHIBAMBALA - Smoked Fish',
             'price'   => 100
         ],
         [
-            'picture' => '/recipe/products/Horace Andayi - Banana Bread (Eshitata)jpg.jpg',
+            'picture' => '/recipe/products/3-Banana-Bread-(Eshitata).jpg',
             'file'    => 'recipes/ESHITATA - Banana Bread.pdf',
             'name'    => 'ESHITATA - Banana Bread',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/5-Boiled-Black-Nightshade-(Esufuwa).jpg',
             'file'    => 'recipes/ESUFUWA - Boiled Black Nightshades.pdf',
             'name'    => 'ESUFUWA - Boiled Black Nightshades',
             'price'   => 100
         ],
         [
-            'picture' => '/recipe/products/Margret Kahenda - Indegenous Chicken (Ingokho).jpg',
+            'picture' => '/recipe/products/9-Free-Range-Chicken-(Ingokho).jpg',
             'file'    => 'recipes/INGOKHO - Free Range Chicken.pdf',
             'name'    => 'INGOKHO - Free Range Chicken',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/2-Bamboo-Shoots-(Kamalea).jpg',
             'file'    => 'recipes/KAMALEA - Bamboo Shoots.pdf',
             'name'    => 'KAMALEA - Bamboo Shoots',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/14-Pumpkin-Leaf-Vegetables-(Lisebebe).jpg',
             'file'    => 'recipes/LISEBEBE - Pumpkin Leaf Vegetables.pdf',
             'name'    => 'LISEBEBE - Pumpkin Leaf Vegetables',
             'price'   => 100
         ],
         [
-            'picture' => '/recipe/products/Evans Asira- Sorghum Porridge.jpg',
+            'picture' => '/recipe/products/17-Sorghum-Porridge-(Obusera-obwa-Amabele).jpg',
             'file'    => 'recipes/OBUSERA OBWA AMABELE - Sorghum Porridge.pdf',
             'name'    => 'OBUSERA OBWA AMABELE - Sorghum Porridge',
             'price'   => 100
         ],
         [
-            'picture' => '/recipe/products/Vennyverah Ivayo - Millet Ugali.jpg',
+            'picture' => '/recipe/products/12-Millet-Ugali-(Obusuma-obwa-Obule).jpg',
             'file'    => 'recipes/OBUSUMA OBWA - Millet Ugali.pdf',
             'name'    => 'OBUSUMA OBWA - Millet Ugali',
             'price'   => 100
         ],
         [
-            'picture' => '/recipe/products/Martha Annet - Mushroom Stew (Obwoba).jpg',
+            'picture' => '/recipe/products/13-Mushroom-Stew-(Obwoba).jpg',
             'file'    => 'recipes/OBWOBA - Mushroom Stew.pdf',
             'name'    => 'OBWOBA - Mushroom Stew',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/11-Jute-_-Cowpeas-Leaves-(Omurere-_-Likhubi).jpg',
             'file'    => 'recipes/Omurere and Likhubi - Jute & Cowpeas.pdf',
             'name'    => 'Omurere and Likhubi - Jute & Cowpeas',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/4-Beans-and-Sweet-Potatoes-Composite-(Omushenye).jpg',
             'file'    => 'recipes/OMUSHENYE - Beans and sweet potato composite.pdf',
             'name'    => 'OMUSHENYE - Beans and sweet potato composite',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/10-Green-Grams-_-Sweet-Potatoes-(Omushenye).jpg',
             'file'    => 'recipes/OMUSHENYE - Green gram and Sweet Potatoes.pdf',
             'name'    => 'OMUSHENYE - Green gram and Sweet Potatoes',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/15-Smoked-Beef-(Shihango).jpg',
             'file'    => 'recipes/SHIHANGO - Smoked Beef.pdf',
             'name'    => 'SHIHANGO - Smoked Beef',
             'price'   => 100
         ],
         [
-            'picture' => '',
+            'picture' => '/recipe/products/8-Dried-Termites-(Tsiswa).jpg',
             'file'    => 'recipes/TSISWA - Dried Termites.pdf',
             'name'    => 'TSISWA - Dried Termites',
             'price'   => 100
