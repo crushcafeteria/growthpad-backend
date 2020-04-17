bbf30a5a7964ea6df7539d9060f6641de31bc213
Nelson Ameyo <nelson@lipasafe.com>
2020-04-17 18:47:18 +0300

    This is an automated deployment

--- resources/views/emails/pesapal-payment-failed.blade.php
+++ resources/views/emails/pesapal-payment-failed.blade.php
@@ -1,11 +1,12 @@
 @component('mail::message')
 # Hello,
 
-It seems your payment via Pesapal failed with the status
-<code>{{$payment->pesapal_status}}</code>
-
+It seems your payment via Pesapal failed with the following status:-
 <br>
-
+STATUS: <code>{{$payment->pesapal_status}}</code>
+<br>
+For more information about this issue, please contact <a href="https://pesapal.com">Pesapal</a>
+<br>
 Thanks,<br>
 {{ config('app.name') }}
 @endcomponent
