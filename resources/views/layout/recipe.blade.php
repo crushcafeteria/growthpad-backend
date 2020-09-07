e8b945c28ae7a32c38baaac88278d4f3904460c8
Nelson Ameyo <nelson@lipasafe.com>
2020-09-07 11:12:22 +0300

    Updating Paypal buttons

--- resources/views/layout/recipe.blade.php
+++ resources/views/layout/recipe.blade.php
@@ -27,8 +27,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
-{{--Facebook Pixel--}}
-<!-- Facebook Pixel Code -->
+    {{--Facebook Pixel--}}
+    <!-- Facebook Pixel Code -->
     <script>
         !function (f, b, e, v, n, t, s) {
             if (f.fbq) return;
@@ -52,8 +52,7 @@
         fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=910947912670238&ev=PageView&noscript=1"
-        /></noscript>
+            src="https://www.facebook.com/tr?id=910947912670238&ev=PageView&noscript=1" /></noscript>
     <!-- End Facebook Pixel Code -->
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -74,67 +73,69 @@
 
 <body>
 
-<script src="https://www.paypal.com/sdk/js?client-id={{ config('cookbook.paypal.client_id') }}&currency={{ (session()->get('locale') == 'de') ? 'EUR' : 'USD' }}&locale={{ (session()->get('locale') == 'de') ? 'de_DE' : 'en_US' }}"></script>
+    <script
+        src="https://www.paypal.com/sdk/js?client-id={{ config('cookbook.paypal.client_id') }}&currency={{ (session()->get('locale') == 'de') ? 'EUR' : 'USD' }}&locale={{ (session()->get('locale') == 'de') ? 'de_DE' : 'en_US' }}">
+    </script>
 
-<!--[if lte IE 9]>
+    <!--[if lte IE 9]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade
     your browser</a> to improve your experience and security.</p>
 <![endif]-->
 
-@php
+    @php
     $cart = ShoppingCart::all();
-@endphp
+    @endphp
 
 
-<!-- header-start -->
-<header>
-    <div class="header-area ">
-        <div id="sticky-header" class="main-header-area ">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-xl-3 col-lg-2">
-                        <div class="logo">
-                            <a href="/">
-                                <img src="/images/logo.png" width="70">
-                            </a>
+    <!-- header-start -->
+    <header>
+        <div class="header-area ">
+            <div id="sticky-header" class="main-header-area ">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-xl-3 col-lg-2">
+                            <div class="logo">
+                                <a href="/">
+                                    <img src="/images/logo.png" width="70">
+                                </a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-xl-9 col-lg-10 float-right">
-                        <div class="main-menu d-none d-lg-block">
-                            <nav>
-                                <ul id="navigation">
-                                    <li><a href="/cookbook"><i class="fa fa-home fa-fw"></i> home</a></li>
-                                    <li><a href="/submit/recipe"><i class="fa fa-paper-plane fa-fw"></i> Submit
-                                            recipe</a></li>
-                                    <li>
-                                        <a target="_blank"
-                                           href="https://play.google.com/store/apps/details?id=com.irenkenya.growthpad.customer.app&hl=en">
-                                            <i class="fa fa-phone fa-fw"></i>Get the app
-                                        </a>
-                                    </li>
+                        <div class="col-xl-9 col-lg-10 float-right">
+                            <div class="main-menu d-none d-lg-block">
+                                <nav>
+                                    <ul id="navigation">
+                                        <li><a href="/cookbook"><i class="fa fa-home fa-fw"></i> home</a></li>
+                                        <li><a href="/submit/recipe"><i class="fa fa-paper-plane fa-fw"></i> Submit
+                                                recipe</a></li>
+                                        <li>
+                                            <a target="_blank"
+                                                href="https://play.google.com/store/apps/details?id=com.irenkenya.growthpad.customer.app&hl=en">
+                                                <i class="fa fa-phone fa-fw"></i>Get the app
+                                            </a>
+                                        </li>
 
-                                    <li>
-                                        <a target="_blank" href="https://irenkenya.com/contact/">
-                                            <i class="fa fa-phone-square fa-fw"></i> Contact
-                                        </a>
-                                    </li>
+                                        <li>
+                                            <a target="_blank" href="https://irenkenya.com/contact/">
+                                                <i class="fa fa-phone-square fa-fw"></i> Contact
+                                            </a>
+                                        </li>
 
-                                    @if(!auth()->check())
+                                        @if(!auth()->check())
                                         <li>
                                             <a href="/register">
                                                 Register
                                             </a>
                                         </li>
-                                    @endif
-                                    <li>
-                                        <a href="/cookbook/cart/display">
-                                            <i class="fa fa-shopping-bag fa-fw"></i> Cart({{ $cart->count() }})
-                                        </a>
-                                    </li>
-                                    @if(auth()->check())
+                                        @endif
+                                        <li>
+                                            <a href="/cookbook/cart/display">
+                                                <i class="fa fa-shopping-bag fa-fw"></i> Cart({{ $cart->count() }})
+                                            </a>
+                                        </li>
+                                        @if(auth()->check())
                                         <li>
                                             <a href="#!" class="dropdown-toggle" data-toggle="dropdown"
-                                               aria-haspopup="true" aria-expanded="false">
+                                                aria-haspopup="true" aria-expanded="false">
                                                 <i class="fa fa-sign-out fa-fw"></i> Hello, {{ auth()->user()->name }}
                                             </a>
                                             <div class="dropdown-menu">
@@ -146,72 +147,74 @@
                                                 </a>
                                             </div>
                                         </li>
-                                    @endif
+                                        @endif
 
 
-                                </ul>
-                            </nav>
+                                    </ul>
+                                </nav>
+                            </div>
                         </div>
                     </div>
-                </div>
 
-            </div>
-        </div>
-    </div>
-</header>
-<!-- header-end -->
-
-@yield('content')
-
-@if($cart->count())
-    <a href="/cookbook/cart/display" class="btn btn-primary btn-lg sticky-btn">Finished shopping? <br>Checkout and pay!</a>
-@endif
-
-<!-- footer  -->
-<footer class="footer mt-5">
-    <div class="copy-right_text">
-        <div class="container">
-            <div class="footer_border"></div>
-            <div class="row align-items-center">
-                <div class="col-xl-12 col-md-12">
-                    <p class="copy_right text-center">
-                        {{ date('Y') }}&copy; All rights reserved
-                    </p>
                 </div>
             </div>
         </div>
-    </div>
-</footer>
-<!--/ footer  -->
+    </header>
+    <!-- header-end -->
 
-<!-- JS here -->
-<script src="/recipe/js/vendor/modernizr-3.5.0.min.js"></script>
-<script src="/recipe/js/vendor/jquery-1.12.4.min.js"></script>
-<script src="/recipe/js/popper.min.js"></script>
-<script src="/recipe/js/bootstrap.min.js"></script>
-<script src="/recipe/js/owl.carousel.min.js"></script>
-<script src="/recipe/js/isotope.pkgd.min.js"></script>
-<script src="/recipe/js/ajax-form.js"></script>
-<script src="/recipe/js/waypoints.min.js"></script>
-<script src="/recipe/js/jquery.counterup.min.js"></script>
-<script src="/recipe/js/imagesloaded.pkgd.min.js"></script>
-<script src="/recipe/js/scrollIt.js"></script>
-<script src="/recipe/js/jquery.scrollUp.min.js"></script>
-<script src="/recipe/js/wow.min.js"></script>
-<script src="/recipe/js/nice-select.min.js"></script>
-<script src="/recipe/js/jquery.slicknav.min.js"></script>
-<script src="/recipe/js/jquery.magnific-popup.min.js"></script>
-<script src="/recipe/js/plugins.js"></script>
-<script src="/recipe/js/gijgo.min.js"></script>
+    @yield('content')
 
-<!--contact js-->
-<script src="/recipe/js/contact.js"></script>
-<script src="/recipe/js/jquery.ajaxchimp.min.js"></script>
-<script src="/recipe/js/jquery.form.js"></script>
-<script src="/recipe/js/jquery.validate.min.js"></script>
-<script src="/recipe/js/mail-script.js"></script>
+    @if($cart->count())
+    <a href="/cookbook/cart/display" class="btn btn-danger btn-lg sticky-btn">Finished shopping? <br>View Cart</a>
+    @endif
 
-<script src="/recipe/js/main.js"></script>
+    <!-- footer  -->
+    <footer class="footer mt-5">
+        <div class="copy-right_text">
+            <div class="container">
+                <div class="footer_border"></div>
+                <div class="row align-items-center">
+                    <div class="col-xl-12 col-md-12">
+                        <p class="copy_right text-center">
+                            {{ date('Y') }}&copy; All rights reserved
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/ footer  -->
+
+    <!-- JS here -->
+    <script src="/recipe/js/vendor/modernizr-3.5.0.min.js"></script>
+    <script src="/recipe/js/vendor/jquery-1.12.4.min.js"></script>
+    <script src="/recipe/js/popper.min.js"></script>
+    <script src="/recipe/js/bootstrap.min.js"></script>
+    <script src="/recipe/js/owl.carousel.min.js"></script>
+    <script src="/recipe/js/isotope.pkgd.min.js"></script>
+    <script src="/recipe/js/ajax-form.js"></script>
+    <script src="/recipe/js/waypoints.min.js"></script>
+    <script src="/recipe/js/jquery.counterup.min.js"></script>
+    <script src="/recipe/js/imagesloaded.pkgd.min.js"></script>
+    <script src="/recipe/js/scrollIt.js"></script>
+    <script src="/recipe/js/jquery.scrollUp.min.js"></script>
+    <script src="/recipe/js/wow.min.js"></script>
+    <script src="/recipe/js/nice-select.min.js"></script>
+    <script src="/recipe/js/jquery.slicknav.min.js"></script>
+    <script src="/recipe/js/jquery.magnific-popup.min.js"></script>
+    <script src="/recipe/js/plugins.js"></script>
+    <script src="/recipe/js/gijgo.min.js"></script>
+
+    <!--contact js-->
+    <script src="/recipe/js/contact.js"></script>
+    <script src="/recipe/js/jquery.ajaxchimp.min.js"></script>
+    <script src="/recipe/js/jquery.form.js"></script>
+    <script src="/recipe/js/jquery.validate.min.js"></script>
+    <script src="/recipe/js/mail-script.js"></script>
+
+    <script src="/recipe/js/main.js"></script>
+
+    @stack('footer-scripts')
 </body>
 
 </html>
